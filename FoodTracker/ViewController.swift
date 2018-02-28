9d68217626199998631fac50c17f475d75a74152
Tyler <tfpauley@gmail.com>
2018-02-28 20:39:20 +0000

    To this point "To Implement the UIText..." on https://developer.apple.com/library/content/referencelibrary/GettingStarted/DevelopiOSAppsSwift/ConnectTheUIToCode.html#//apple_ref/doc/uid/TP40015214-CH22-SW1

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field user input through delegate callbacks.
+        // self refers to the ViewController since it is defined inside (same as 'this' in java)
+        nameTextField.delegate = self
     }
 
-
-
+//MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        <#code#>
+    }
+//MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+      mealNameLabel.text = "Default Text"
+    }
 }
+
 
